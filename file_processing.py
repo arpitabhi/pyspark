1a336024529c146668f7715f959414ad0b44e3e8
ARPIT SHUKLA <47503958+arpitabhi@users.noreply.github.com>
2021-04-22 04:29:33 +0530

    Add files via upload

---
+++ file_processing.py
@@ -0,0 +1,40 @@
+# pyspark_job.py
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+def create_spark_session():
+    """Create spark session.
+Returns:
+        spark (SparkSession) - spark session connected to AWS EMR
+            cluster
+    """
+    spark = SparkSession \
+        .builder \
+        .master("local") \
+        .getOrCreate()
+    return spark
+def process_emp_data(spark, input_path, output_path):
+    """
+    Process the Employee data spread accross different folders and write to S3 as 5 different csv files.
+        Arguments:
+            spark (SparkSession) - spark session connected to AWS EMR
+                cluster
+            input_path (str) - AWS S3 bucket path for source data
+            output_path (str) - AWS S3 bucket for writing processed data
+    """
+    input_path_schema=input_path+'/emp*with_schema.csv'
+    input_path_without_schema=input_path+'/emp*without_schema.csv'
+    df = spark.read.options(header='True', delimiter=',').csv(input_path_schema)
+    df_without_schema = spark.read.options(header='False',delimiter=",").csv(input_path_without_schema)
+    # Apply some basic filters and aggregate by department_code.
+	
+    # Save the data to your S3 bucket as a .csv file.
+    df_union = df.union(df_without_schema)
+    df_union.repartition("department_code").write.option("header",True).partitionBy("department_code").mode('overwrite').csv(output_path)
+	
+def main():
+    spark = create_spark_session()
+    input_path = ('C:/Users/arpit/Desktop/s3/emp*')
+    output_path = 'C:/Users/arpit/Desktop/s3_2'
+    process_emp_data(spark, input_path, output_path)
+if __name__ == '__main__':
+    main()
